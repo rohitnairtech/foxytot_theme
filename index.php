d7d2b7b969bbc09c9fd16d69348bfbd10a434973
Siddharth <siddh0662@gmail.com>
2018-07-03 10:53:37 +0530

    Fixed category tiles alignment

--- index.php
+++ index.php
@@ -173,7 +173,7 @@
                 <div class="mbr-gallery-row">
                     <div class="mbr-gallery-layout-default">
                         <div>
-                            <div>
+                            <div class="tiles">
                                 <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Awesome">
                                     <div href="#lb-gallery1-z" data-slide-to="0" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery00.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Featured</span></div>
                                 </div>
@@ -187,64 +187,31 @@
                                     <div href="#lb-gallery1-z" data-slide-to="3" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery03.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Bobby pins</span></div>
                                 </div>
                                 <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Awesome">
-                                    <div href="#lb-gallery1-z" data-slide-to="4" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery04.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Claw Clips</span></div>
-                                </div>
-                                <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Awesome">
-                                    <div href="#lb-gallery1-z" data-slide-to="5" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery05.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Party Accessories</span></div>
+                                    <div href="#lb-gallery1-z" data-slide-to="0" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery00.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Featured</span></div>
                                 </div>
                                 <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Responsive">
-                                    <div href="#lb-gallery1-z" data-slide-to="6" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery06.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Tiaras</span></div>
+                                    <div href="#lb-gallery1-z" data-slide-to="1" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery01.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Hair bands</span></div>
+                                </div>
+                                <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Creative">
+                                    <div href="#lb-gallery1-z" data-slide-to="2" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery02.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Banana Clips</span></div>
                                 </div>
                                 <div class="mbr-gallery-item mbr-gallery-item--p1" data-video-url="false" data-tags="Animated">
-                                    <div href="#lb-gallery1-z" data-slide-to="7" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery07.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Hair elastics</span></div>
+                                    <div href="#lb-gallery1-z" data-slide-to="3" data-toggle="modal"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery03.jpg" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Bobby pins</span></div>
                                 </div>
+
+                                
                             </div>
                         </div>
                         <div class="clearfix"></div>
                     </div>
                 </div>
-                <!-- Lightbox -->
-                <div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery1-z">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-body">
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery00.jpg" alt="" title=""></div>
-                                    <div class="carousel-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery01.jpg" alt="" title=""></div>
-                                    <div class="carousel-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery02.jpg" alt="" title=""></div>
-                                    <div class="carousel-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery03.jpg" alt="" title=""></div>
-                                    <div class="carousel-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery04.jpg" alt="" title=""></div>
-                                    <div class="carousel-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery05.jpg" alt="" title=""></div>
-                                    <div class="carousel-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery06.jpg" alt="" title=""></div>
-                                    <div class="carousel-item"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/gallery07.jpg" alt="" title=""></div>
-                                </div><a class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#lb-gallery1-z"><span class="mbri-left mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control carousel-control-next" role="button" data-slide="next" href="#lb-gallery1-z"><span class="mbri-right mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Next</span></a><a class="close" href="#" role="button" data-dismiss="modal"><span class="sr-only">Close</span></a></div>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
 
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+        
+
+        
 
     </section>
 
