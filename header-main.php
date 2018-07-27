533cfdd3414382023aa9827bc08ab975102110d1
Rohit <puthenrohit@gmail.com>
2018-07-27 11:14:53 +0530

    Removed the lower navbar button issue

--- header-main.php
+++ header-main.php
@@ -150,7 +150,7 @@ $('.navbar-lower').affix({
 
 <nav class="d-block d-sm-none navbar nav-responsive navbar-collapse-lg" id="navResponsive">
   <div class="navbar-title" href="#"> 
-  <a class="navbar-toggler secondary" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #ff6600;">
+  <a class="navbar-toggler secondary" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="background-color: #ff6600;">
   	Menu
     <span><i class="fas fa-sort-down fa-1x" id="upDown"></i></span>
   </a>
